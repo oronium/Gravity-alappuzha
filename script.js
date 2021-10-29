9a35b26bfe4ee85a0318740e835ae359a25804f7
Christin36 <89018583+Christin36@users.noreply.github.com>
2021-10-29 14:46:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// const navFunc = () => {
-//   const burger = document.querySelector(".burger");
-//   const nav = document.querySelector(".nav-list");
-//   const bg = document.querySelector(".main-nav");
-
-//   burger.addEventListener("click", () => {
-//     nav.classList.toggle("nav-active");
-//     bg.classList.toggle("nav-background");
-//   });
-// };
-// navFunc();
 const barFunc = function () {
   const burger = document.querySelector(".burger");
   const line1 = document.querySelector(".line-1");
